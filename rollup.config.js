c81e39cdbde65bc40bbe1ffcf40451a398c81360
Алексей Кузнецов <mirakuru@webtoucher.ru>
2020-09-25 21:45:57 +0700

    First build

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,20 @@
 // rollup.config.js
-import resolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import { uglify } from "rollup-plugin-uglify"
 
 export default {
-  input: './src/vue-touch-scrollbar.js',
-  output: {
-    file: './dist/vue-touch-scrollbar.js',
-    format: 'umd',
-    name: 'vue-touch-scrollbar'
-  },
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**' // only transpile our source code
-    })
-  ]
-};
+    input: './src/vue-touch-scrollbar.js',
+    output: {
+        file: './dist/vue-touch-scrollbar.js',
+        format: 'umd',
+        name: 'vue-touch-scrollbar',
+    },
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**', // only transpile our source code
+        }),
+        uglify()
+    ],
+}
